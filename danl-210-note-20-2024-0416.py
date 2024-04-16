26b53d50ca3915371beed8f59cf77fba553ee3e9
bcdanl <bchoe@geneseo.edu>
2024-04-16 19:41:34 -0400

    lec20

---
+++ danl-210-note-20-2024-0416.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 16:33:44 2024
+
+@author: bchoe
+"""
+
+# %%
+# =============================================================================
+# Python libraries
+# =============================================================================
+import pandas as pd
+
+
+# %%
+# =============================================================================
+# pd.read_table
+# =============================================================================
+url = "https://www.nps.gov/orgs/1207/national-park-visitation-sets-new-record-as-economic-engines.htm"
+
+tables = pd.read_html(url)
+len(tables)
+tables[0]
+tables[1]
